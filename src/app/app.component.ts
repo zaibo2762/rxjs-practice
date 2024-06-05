60e41f5b995ec5a3a295ef9c2262b51a6c657b13
zaibo2762 <77626531+zaibo2762@users.noreply.github.com>
2024-06-05 15:14:39 +0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-
+import { Observable, Subscription, debounceTime, filter, fromEvent, map, shareReplay, switchMap, tap } from 'rxjs';
+import {AjaxResponse,ajax} from 'rxjs/ajax'
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -9,6 +10,68 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'rxjs-practice';
+export class AppComponent implements OnInit {
+  @ViewChild("Search",{static:true}) 
+  Search?:ElementRef<HTMLInputElement>
+  users:any = []
+  typeAheadsub?:Subscription
+
+ ngOnInit(){
+  const searchObs = fromEvent(this.Search!.nativeElement,"input")
+                    .pipe(
+                      debounceTime(2000),
+                      filter((e:any)=>e.target.value != ""),
+                      switchMap((e:any)=>{
+                      return ajax(`https://api.github.com/search/users?q=${e.target.value}`)
+                      }),
+                      map((AjaxResponse:any)=>AjaxResponse.response.items)
+                    )
+ this.typeAheadsub = searchObs.subscribe((value:any)=>{
+    //console.log(value)
+    this.users = value
+  })
+
+
+
+
+  //Observable
+   const pizzaObservable =new Observable((subscriber)=>{
+    console.log("inside observeable")
+    subscriber.next({name:"Farm House",veg:true,size:"small"})
+    // subscriber.next({name:"Margerita",veg:true,size:"large"})
+    // subscriber.next({name:"bar b q Chicken",veg:false,size:"extra large"})
+    subscriber.complete()
+   }).pipe(
+    tap((pizza:any)=>{
+      console.log('inside pipe')
+      //side effects
+      // if(pizza.size == "large"){
+      //   throw new Error("Large size pizza are not Available")
+      // }
+    }),
+    filter((pizza:any)=>pizza.veg==true),
+    map((pizza:any)=> pizza.name),
+    shareReplay()
+   )
+
+   //Subscriber
+   pizzaObservable.subscribe(
+    (value)=>console.log(value),
+    (err)=>console.log(err.message),
+    ()=>console.log('I am done eating pizza')
+   )
+   pizzaObservable.subscribe(
+    (value)=>console.log(value),
+    (err)=>console.log(err.message),
+    ()=>console.log('I am done eating pizza')
+   )
+ }
+
+ ngOnDestroy(){
+  if(this.typeAheadsub){
+    this.typeAheadsub.unsubscribe();
+  }
+ }
 }
+
+
